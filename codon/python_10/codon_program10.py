1afdf3781c30af200497b72e47746d2c15b21aa1
chaforinli <457535344@qq.com>
2023-04-11 18:42:48 +0800

    Update codon_program10.py

--- codon/python_10/codon_program10.py
+++ codon/python_10/codon_program10.py
@@ -1,8 +1,8 @@
-class ZgM3gNx202 (): 
-	q1Z69u23 = 31
+class ZgM3gNx202: 
+	q1Z69u23: float = 31.0
 	def eih319224 ( self )  : 
 			u0r65y5c8 = 6960 
-			self.q1Z69u23/=(1if((1if(self.q1Z69u23==0)else(self.q1Z69u23))==0)else((1if(self.q1Z69u23==0)else(self.q1Z69u23)))) ; 
+			self.q1Z69u23/=(1if((1if(self.q1Z69u23==0)else(self.q1Z69u23))==0)else((1if(self.q1Z69u23==0)else(self.q1Z69u23)))) 
 if __name__ == '__main__':
 	po0hER8fZ= ZgM3gNx202();
 	po0hER8fZ.eih319224()
