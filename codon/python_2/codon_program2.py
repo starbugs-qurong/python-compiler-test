c2f9cdfff408bb0fa2339c98a394252f8d16abdc
chaforinli <457535344@qq.com>
2023-04-11 17:34:14 +0800

    Update codon_program2.py

--- codon/python_2/codon_program2.py
+++ codon/python_2/codon_program2.py
@@ -1,7 +1,7 @@
-class O8Y694p3o (): 
-	a3e16X35 = "CgtSETQBaCgC"
-	def L62326a80a ( self , XY7C5a3Z : bytes ) -> bytes : 
-			if ((True)or(False))or((("sR2Kt7"))==(self.a3e16X35)) : 
+class O8Y694p3o: 
+	a3e16X35: str = "CgtSETQBaCgC"
+	def L62326a80a ( self , XY7C5a3Z : int ) -> int : 
+			if ((True) or (False)) or ((("sR2Kt7"))==(self.a3e16X35)) : 
 				B06Cx4V2 = "vAl_r1iUA_H" ; 
 if __name__ == '__main__':
 	Qokhsc8W = 28
